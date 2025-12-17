7b743a243a9f131463a97900e33a2cf6b59a9996
k128c <aeth.kr@gmail.com>
2025-12-16 23:27:56 -0600

    Clean up code
    Place constants at top of file, and divide most of the program
    into functions for better organization

--- search.py
+++ search.py
@@ -2,50 +2,69 @@ import os
 import requests
 from dotenv import load_dotenv, dotenv_values
 
+# variables
 load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-url = 'https://id.twitch.tv/oauth2/token'
-data = {
-    'client_id':CLIENT_ID,
-    'client_secret':CLIENT_SECRET,
-    'grant_type':'client_credentials'
-}
+TOKEN_URL = 'https://id.twitch.tv/oauth2/token'
+GAME_URL = 'https://api.igdb.com/v4/games/'
 
-response = requests.post(url, json=data)
-r_json = response.json()
+IMPORTANT_FIELDS = "id, name, game_type.type, rating, summary"
+EXCLUDED_FIELDS = ['name', 'game_type', 'rating']
 
-ACCESS_TOKEN = r_json['access_token']
+# functions
+def get_access_token() -> str:
+    data = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'grant_type': 'client_credentials'
+    }
 
-headers = {
-    'Client-ID': CLIENT_ID,
-    'Authorization': f'Bearer {ACCESS_TOKEN}'
-}
+    response = requests.post(TOKEN_URL, json=data)
+    try:
+        response.raise_for_status
+    except Exception as e:
+        print (e)
+    else:
+        return response.json()['access_token']
 
-search_key = input("Enter game name: ")
+def get_headers() -> dict:
+    return {
+        'Client-ID': CLIENT_ID,
+        'Authorization': f'Bearer {get_access_token()}'
+    }
 
-important_fields = "id, name, game_type.type"
-query = f'''
-    fields {important_fields}, rating, storyline, summary;
-    search "{search_key}";
-    where game_type != (13, 5, 3) & version_parent = null & themes != (42) & rating != 0;
-    limit 1;
-'''
+def return_game_info(game_name: str) -> any:
+    query = f'''
+        fields {IMPORTANT_FIELDS};
+        search "{game_name}";
+        where game_type != (13, 5, 3) & version_parent = null & themes != (42) & rating != 0;
+        limit 1;
+    '''
+    response = requests.post(GAME_URL, headers=get_headers(), data=query)
+    return response.json()
 
-game_url = 'https://api.igdb.com/v4/games/'
-response = requests.post(game_url, headers=headers, data=query)
-g_json = response.json()
+def main() -> None:
+    # search for game
+    search_key = input("Enter game name: ")
+    game_info = return_game_info(search_key)
 
-excluded_fields = ['name', 'game_type', 'rating']
-try:
-    for e in g_json:
-        print(f"{e['name']}")
-        for key, value in e.items():
-            if key == 'rating':
-                print(f"  {key}: {value:.2f}")
-            if key not in excluded_fields:
-                print(f"  {'IGDB ID' if key=='id' else key}: {value}")
-except Exception as e:
-    print(e)
+    try:
+        no_games_returned: bool = len(game_info) == 0
+        if (no_games_returned):
+            raise Exception("No games were returned. Perhaps search under different terms?")
+        else:
+            for e in game_info:
+                print(f"{e['name']}")
+                for key, value in e.items():
+                    if key == 'rating':
+                        print(f"  {key}: {value:.2f}")
+                    if key not in EXCLUDED_FIELDS:
+                        print(f"  {'IGDB ID' if key=='id' else key}: {value}")
+    except Exception as e:
+        print(e)
+
+if __name__ ==  '__main__':
+    main()
